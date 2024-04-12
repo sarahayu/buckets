6749a5ea2a3f0ad65e47a3d6ac6e4f52e90dc008
sarahayu <46186848+sarahayu@users.noreply.github.com>
2024-04-11 21:31:23 -0700

    vert option and deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import BucketViz from "./BucketViz";
 import DotPDF from "./DotPDF";
 import LineGraph from "./LineGraph";
 import { deltaData } from "./data";
+import DotPDFVert from "./DotPDFVert";
 
 const scenKeys = Object.keys(deltaData);
 
 export default function App({}) {
   const scenCounter = useRef(0);
+  const [vert, setVert] = useState(false);
   const [curScen, setCurScen] = useState(scenKeys[0]);
 
   return (
@@ -15,9 +17,18 @@ export default function App({}) {
       <button onClick={() => void setCurScen(scenKeys[++scenCounter.current])}>
         {curScen}
       </button>
+      <br />
+      <span>Vertical</span>
+      <input
+        type="checkbox"
+        name="vert"
+        id="vert"
+        value={vert}
+        onChange={(e) => void setVert((d) => !d)}
+      />
       <LineGraph curScen={curScen} />
       <BucketViz curScen={curScen} />
-      <DotPDF curScen={curScen} />
+      {vert ? <DotPDFVert curScen={curScen} /> : <DotPDF curScen={curScen} />}
     </>
   );
 }
---
+++ src/DotPDFVert.jsx
@@ -0,0 +1,188 @@
+import { useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
+import { deltaData } from "./data";
+
+export default function DotPDFVert({ curScen }) {
+  const svgElem = useRef();
+
+  const margin = { top: 10, right: 30, bottom: 30, left: 750 },
+    width = 400,
+    height = 400;
+
+  useEffect(() => {
+    const svg = d3
+      .select(svgElem.current)
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("class", "graph-area")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const x = d3.scaleLinear().domain([0, 80]).range([0, width]);
+    const y = d3.scaleLinear().domain([0, 400]).range([height, 0]);
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom().scale(x));
+    const y_axis = svg.append("g");
+    y_axis.transition().duration(500).call(d3.axisLeft().scale(y));
+
+    svg
+      .append("path")
+      .attr(
+        "d",
+        d3.line(
+          (d) => x(d[0]),
+          (d) => y(d[1])
+        )([
+          [0, 200],
+          [80, 200],
+        ])
+      )
+      .attr("stroke", "gray");
+
+    svg
+      .append("text")
+      .text("test")
+      .style("font-size", 11)
+      .attr("class", "left-text")
+      .attr("text-anchor", "middle")
+      .attr("transform", `translate(${200},${300})`);
+
+    svg
+      .append("text")
+      .text("test2")
+      .style("font-size", 11)
+      .attr("class", "right-text")
+      .attr("text-anchor", "middle")
+      .attr("transform", `translate(${200},${100})`);
+  }, []);
+
+  useEffect(() => {
+    const data = deltaData[curScen];
+    const svg = d3.select(svgElem.current).select(".graph-area");
+    const x = d3.scaleLinear().domain([0, 80]).range([0, width]);
+    const y = d3.scaleLinear().domain([0, 400]).range([height, 0]);
+
+    const histogram = d3
+      .histogram()
+      .value((d) => d)
+      .domain(y.domain())
+      .thresholds(
+        d3.range(25).map((i) => d3.scaleLinear().range(y.domain())(i / 24))
+      );
+
+    const bins = histogram(data);
+    // console.log(bins);
+
+    const u = svg.selectAll("rect").data(bins);
+
+    u.join("rect")
+      .transition()
+      .duration(500)
+      .attr("y", 1)
+      .attr("transform", function (d) {
+        return `translate(${x(0)}, ${y(d.x1)})`;
+      })
+      .attr("height", function (d) {
+        return y(d.x0) - y(d.x1) - 1;
+      })
+      .attr("width", function (d) {
+        return x(Math.round(d.length));
+      })
+      .style("fill", "gray")
+      .attr("opacity", 0.2);
+
+    function minusLeast(arr) {
+      let indMost = -1;
+      let most = -1;
+
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != 0 && arr[i] - bins[i].length / 4 > most) {
+          indMost = i;
+          most = arr[i] - bins[i].length / 4;
+        }
+      }
+
+      arr[indMost] -= 1;
+      return arr;
+    }
+
+    function addMost(arr) {
+      let indMost = -1;
+      let most = -1;
+
+      for (let i = 0; i < arr.length; i++) {
+        if (bins[i].length / 4 - arr[i] > most) {
+          indMost = i;
+          most = bins[i].length / 4 - arr[i];
+        }
+      }
+
+      arr[indMost] += 1;
+      return arr;
+    }
+
+    let rad = 8;
+
+    function getCircs(bbins) {
+      let circBins = [];
+      let sum = 0;
+      for (let bin of bbins) {
+        let rounded = Math.round(bin.length / 4);
+        circBins.push(rounded);
+        sum += rounded;
+      }
+
+      while (sum > 20) {
+        circBins = minusLeast(circBins);
+
+        sum = 0;
+        for (let bin of circBins) {
+          let rounded = bin;
+          sum += rounded;
+        }
+      }
+
+      while (sum < 20) {
+        circBins = addMost(circBins);
+
+        sum = 0;
+        for (let bin of circBins) {
+          let rounded = bin;
+          sum += rounded;
+        }
+      }
+
+      let circs = [];
+      for (let x = 0; x < circBins.length; x++) {
+        for (let y = 0; y < circBins[x]; y++) {
+          circs.push([(bins[x].x1 + bins[x].x0) / 2, y * 4 + 2]);
+        }
+      }
+
+      return circs;
+    }
+    const circData = getCircs(bins);
+    const v = svg.selectAll("circle").data(circData, (d, i) => i);
+
+    v.join("circle")
+      .transition()
+      .duration(500)
+      .attr("cx", (d) => x(d[1]))
+      .attr("cy", (d) => y(d[0]))
+      .attr("r", rad)
+      .attr("fill", (d) =>
+        d[0] < 200 ? d3.interpolateReds((200 - d[0]) / 400 + 0.5) : "steelblue"
+      );
+
+    const amtLeft = circData.filter((d) => d[0] < 200).length;
+
+    svg.select(".left-text").text(`${amtLeft} years / 20 WILL NOT meet demand`);
+    svg
+      .select(".right-text")
+      .text(`${20 - amtLeft} years / 20 WILL meet demand`);
+  }, [curScen]);
+
+  return <svg ref={svgElem}></svg>;
+}
